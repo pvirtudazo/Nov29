fcb39a70d8763dfe606de4805c6a3fdc188829be
nikhilKapse7 <45395116+nikhilKapse7@users.noreply.github.com>
2018-11-29 10:28:54 -0500

    Update Nov29.java
    Fixed mistype in convertToArrayList.

--- Nov29.java
+++ Nov29.java
@@ -4,7 +4,7 @@ public class Nov29
 
 	public static ArrayList convertToArrayList(int n) {
 		ArrayList<Integer> list = new ArrayList<>();
-		for(int i = 0; n < 0; i++) {
+		for(int i = 0; n > 0; i++) {
 			list.add(0, (n % 10));
 			n = n / 10;
 		}
