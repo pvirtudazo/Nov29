d81728277015fce7b51b0ba93750df8e410c0e11
nikhilKapse7 <45395116+nikhilKapse7@users.noreply.github.com>
2018-11-29 10:19:28 -0500

    Update Nov29.java
    Added convertToArrayList method

--- Nov29.java
+++ Nov29.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
 public class Nov29
 {
+	public static ArrayList convertToArrayList(int n) {
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i = 0; n < 0; i++) {
+			list.add(0, (n % 10));
+			n = n / 10;
+		}
+		return list;
+	}
 	public static void main(String[] args)
 	{
 	}
