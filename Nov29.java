d9f956314827cac18117706ef1282769bed5a24f
pvirtudazo <pvirtudazo7@gmail.com>
2018-11-29 10:16:41 -0500

    Nov29 Update
    convertFromArrayList method

--- Nov29.java
+++ Nov29.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
 public class Nov29
 {
+	public static int convertFromArrayList(ArrayList<Integer> lista)
+	{
+		int or=0;
+		int count=1;
+		for (int i=lista.size();i>0;i--)
+		{
+			or+=(lista.get(i-1)*count);
+			count*=10;
+		}
+		return or;
+	}
 	public static void main(String[] args)
 	{
 	}
